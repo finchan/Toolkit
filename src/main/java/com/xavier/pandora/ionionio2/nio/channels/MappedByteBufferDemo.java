2d5bb27e977be3c0ccec58366bf56ca63b3fca46
finchan <finchan@outlook.com>
2021-01-05 15:19:28 +0800

    MappedByteBuffer Sample

--- src/main/java/com/xavier/pandora/ionionio2/nio/channels/MappedByteBufferDemo.java
+++ src/main/java/com/xavier/pandora/ionionio2/nio/channels/MappedByteBufferDemo.java
@@ -21,16 +21,34 @@ public class MappedByteBufferDemo {
         logger.info("File Channel Size: " + size);
         logger.info("File Channel Position: " + fc.position());
         MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_WRITE, 0, size);
-        logger.info("MappedByteBuffer's info" + mbb);
+        logger.info("1. MappedByteBuffer's info" + mbb);
         String fileContent = "";
         while(mbb.hasRemaining()) {
             fileContent += (char)mbb.get();
         }
+        logger.info("After get Data: " + mbb);
         logger.info("File Content: \n" + fileContent);
         logger.info("-------------------");
         for (int i = 0; i < mbb.limit()/2; i++) {
-
+            byte b1 = mbb.get(i);
+            logger.info("mbb get i - position: " + mbb.position());
+            byte b2 = mbb.get(mbb.limit()-i-1);
+            logger.info("mbb get limit()-i-1 - position: " + mbb.position());
+            mbb.put(i, b2);
+            logger.info("mbb put i - position: " + mbb.position());
+            mbb.put(mbb.limit()-i-1, b1);
+            logger.info("mbb put limit()-i-1 - position: " + mbb.position());
+            logger.info("LOOPED - MappedByteBuffer's info" + mbb);
         }
+        mbb.flip();
+        logger.info("2. MappedByteBuffer's info" + mbb);
+        String updateFileContent = "";
+        while(mbb.remaining()>0) {
+            updateFileContent += (char) mbb.get();
+            logger.info("mbb get() - position: " + mbb.position());
+            logger.info("mbb get() - limit: " + mbb.limit());
+        }
+        logger.info("\n"+updateFileContent);
         fc.close();
     }
 }
